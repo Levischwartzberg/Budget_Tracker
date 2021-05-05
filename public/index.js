63163fc2aeb07c314f0528ccb0b68ffca6aed4ca
Levi Schwartzberg <Levischwartzberg@gmail.com>
2021-05-05 14:58:24 -0500

    added service worker link to index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,13 @@
+//added service-worker linking
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((reg) => {
+        console.log('Service worker registered.', reg);
+      });
+  });
+}
+
 let transactions = [];
 let myChart;
 
